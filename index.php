2f2f121f6b8d593aaf776f80a81ed1af48158fcf
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-07 22:30:26 -0700

    working on gloop. didnt feel the need to push it. reworking comments.

--- index.php
+++ index.php
@@ -69,6 +69,7 @@
 		<script type="text/javascript" src="js/entities/SpearThrow.js"></script>
 		<script type="text/javascript" src="js/entities/MiniMap.js"></script>
 		<script type="text/javascript" src="js/entities/MiniPlayerLocation.js"></script>
+		<script type="text/javascript" src="js/entities/Gloop.js"></script>
 		<script type="text/javascript" src="js/screens/title.js"></script>
 		<script type="text/javascript" src="js/screens/play.js"></script>
 		<script type="text/javascript" src="js/screens/spendExp.js"></script>
